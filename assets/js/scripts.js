63fac8370f36c8e0c980b6eaf86c49b898acb642
Konstantin <mogilevkostya@yandex.ru>
2025-09-05 00:29:02 +0700

    Скрипты: Добавление проверок

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,10 +3,11 @@ import { initSwiper } from './scripts/sliders.js';
 import { initSelect } from './scripts/select.js';
 import { initShowMore } from './scripts/showMore.js';
 import './scripts/animations.js';
-import './scripts/quiz.js';
-// import './modules/spollers/spollers.js';
+import { initQuiz } from './scripts/quiz.js';
+import './modules/spollers/spollers.js';
 
 initHeader();
 initSwiper();
 initSelect();
 initShowMore();
+initQuiz();
